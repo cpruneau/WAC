44f66fc24eac85e2d2381cc161c631fdbfd368b9
yravan <71646380+yravan@users.noreply.github.com>
2020-10-15 22:28:48 -0400

    Update RunPythiaSimulation.cpp

--- Pythia/RunPythiaSimulation.cpp
+++ Pythia/RunPythiaSimulation.cpp
@@ -30,8 +30,8 @@ int main()
   cout << "<INFO> PYTHIA Model Analysis - Starting" << endl;
 
 //  long nEventsRequested = 100;
-  long nEventsRequested = 10000;
-  int  nEventsReport    = 100;
+  long nEventsRequested = 1000000;
+  int  nEventsReport    = 10000;
 
 
 
